c76e478bea5e1e2dca442cdba3c73d0ec23f1e49
coldsandwichmeat <be938598@gmail.com>
2026-08-13 08:01:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cdgvhvrdubge.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheongkaiwen-employee"
+customregion = "us-east-1"
+
