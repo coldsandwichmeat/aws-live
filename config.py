88a0552f2aa079b6afec6bc30181ce811cf314ca
coldsandwichmeat <be938598@gmail.com>
2026-08-13 08:16:27 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.cdgvhvrdubge.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "cheongkaiwen-employee"
-customregion = "us-east-1"
-
